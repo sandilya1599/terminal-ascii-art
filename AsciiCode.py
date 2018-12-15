81cf09b9a26df4432ba1cc10ee785167ffcfa2d9
sandilya1599 <33326061+sandilya1599@users.noreply.github.com>
2018-12-15 18:48:25 +0530

    Add files via upload

--- AsciiCode.py
+++ AsciiCode.py
@@ -27,9 +27,8 @@ def search(fileName):
 	currentDir=os.getcwd()
 	files=os.listdir(currentDir)
 	if fileName in files:
-		found=True	
+		found=True
 	else:
-		#print(unexplored,files)
 		findFile(fileName,files)
 		while len(unexplored) >0 and found == False:
 			nowCurrent=os.path.abspath(unexplored.pop(0))
@@ -39,15 +38,15 @@ def search(fileName):
 					findFile(fileName,files)
 				else:
 					found=True
-					path=nowCurrent	
+					path=nowCurrent
 			except FileNotFoundError:
 				continue	
 				if found:
-					break					
+					break
 	if found:
 		return path
 	else:
-		raise FileNotFoundError			
+		raise FileNotFoundError	
 fileName=input("Enter JPEG/PNG file name with extenstion:")
 imagePath=search(fileName)+"/"+fileName
 image=Image.open(imagePath)
@@ -55,12 +54,11 @@ image=image.resize((100,100))
 #Image format and size
 print("The format of the image is ",image.format)
 print("The size of image is ",image.size[0],"X",image.size[1])
-#image.show()
 
 #import numpy and use its array to convert image to array
 matrix=array(image)
 
-# find the no of rows and columns 
+# find the no of rows and columns
 rows,cols=len(matrix),len(matrix[1])
 #brightness of each pixel 
 brightness=zeros(shape=(rows,cols),dtype=int)
@@ -71,16 +69,14 @@ for row in range(rows):
 print("Constructed Brightness matrix")
 
 mapped="`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-#print("Brightness of first pixel is",brightness[0][0])        
 asciiBrightness=empty(shape=(rows,cols),dtype="<U2")
 
-"""approximation 
+"""approximation
 256 numbers to 65 characters
 which means 4 numbers to each character"""
 for row in range(rows):
     for col in range(cols):
         asciiBrightness[row][col]=mapped[int(round(brightness[row][col]/65))]
-
 
 for row in range(rows):
     for col in range(cols):
